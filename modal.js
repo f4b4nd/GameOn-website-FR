e3a4df5eb9544c0530c063b6d47f1d372392bc61
fabien <fabien.andrian@gmail.com>
2022-02-23 11:02:12 +0100

    set button close modal

--- modal.js
+++ modal.js
@@ -11,6 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeModalBtn = modalbg.querySelector(".close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +21,19 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal
+function closeModal() {
+  modalbg.style.display = "none";
+}
 
+// close event
+closeModalBtn.addEventListener('click', closeModal);
+
+
+// check input with regex: nom, prenom, mail, nombre de tournois
+
+// get values from input
+
+// redirect when clicked
+
+// close modal
